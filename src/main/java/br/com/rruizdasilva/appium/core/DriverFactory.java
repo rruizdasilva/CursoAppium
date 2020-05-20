8a58f906e95afda7195f1b4d9f2ddca37b1ae834
Roger <roger.dasilva@ul.com>
2020-05-20 16:06:03 -0300

    Incluindo driver para testar na nuvem

--- src/main/java/br/com/rruizdasilva/appium/core/DriverFactory.java
+++ src/main/java/br/com/rruizdasilva/appium/core/DriverFactory.java
@@ -13,7 +13,10 @@ public class DriverFactory {
     private static AndroidDriver<MobileElement> driver;
 
     public static AndroidDriver<MobileElement> getDriver() {
-        if (driver == null) createDriver();
+        if (driver == null) {
+            createDriver();
+            // createTestObjectDriver();
+        }
         return driver;
     }
 
@@ -26,6 +29,20 @@ public class DriverFactory {
 
         try {
             driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createTestObjectDriver() {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("testobject_api_key", "90A2E47EA0C54BC3B2300547C77ADC3A");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appiumVersion", "1.7.2");
+
+        try {
+            driver = new AndroidDriver<MobileElement>(new URL("https://us1.appium.testobject.com/wd/hub"), desiredCapabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
